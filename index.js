fbc21d853be422280d55e7178c1d96fcd911e537
Leonardo Silveira <sombriks@gmail.com>
2016-07-17 17:30:20 -0300

    rotas de serviço

--- index.js
+++ index.js
@@ -1,16 +1,98 @@
 // imports 
 var express = require("express");
-var app     = express();
+var app = express();
 
-var cfg     = require("./knexfile");
-var knex    = require("knex")(cfg.development);
+var cfg = require("./knexfile");
+var knex = require("knex")(cfg.development);
 
 // static files
 app.use(express.static("www"));
 
+app.get("/festa/list", function (req, res) {
+  knex("vw_festa").select().then(function (ret) {
+    res.send(ret);
+  });
+});
+
+app.get("/festa/:idfesta", function (req, res) {
+  knex("vw_festa").select().where({ idfesta: req.params.idfesta }).then(function (ret) {
+    res.send(ret[0]);
+  });
+});
+
+app.post("/festa/save", function (req, res) {
+  var festa = req.body;
+  knex("festa").insert(festa, "idfesta").then(function (ret) {
+    festa.idfesta = ret[0];
+    res.send(festa);
+  });
+});
+
+app.put("/festa/save", function (req, res) {
+  var festa = req.body;
+  knex("festa").update(festa).where({ idfesta: festa.idfesta }).then(function (ret) {
+    res.send(festa);
+  })
+});
+
+app.delete("/festa/:idfesta", function (req, res) {
+  knex("festa").del().where({ idfesta: req.params.idfesta }).then(function () {
+    res.send("OK");
+  })
+});
+
+app.get("/convidado/list", function (req, res) {
+  knex("vw_convidado").select().then(function (ret) {
+    res.send(ret);
+  });
+});
+
+app.get("/convidado/:idconvidado", function (req, res) {
+  knex("vw_convidado").select().where({ idconvidado: req.params.idconvidado }).then(function (ret) {
+    res.send(ret[0]);
+  });
+});
+
+app.post("/convidado/save", function (req, res) {
+  var convidado = req.body;
+  knex("convidado").insert(convidado, "idconvidado").then(function (ret) {
+    convidado.idconvidado = ret[0];
+    res.send(convidado);
+  });
+});
+
+app.put("/convidado/save", function (req, res) {
+  var convidado = req.body;
+  knex("convidado").update(convidado).where({ idconvidado: convidado.idconvidado }).then(function (ret) {
+    res.send(convidado);
+  })
+});
+
+app.delete("/convidado/:idconvidado", function (req, res) {
+  knex("convidado").del().where({ idconvidado: req.params.idconvidado }).then(function () {
+    res.send("OK");
+  })
+});
+
+app.post("/convidar", function (req, res) {
+  // {idfesta:1,idconvidado:1}
+  knex("festa_convidado").insert(req.body).then(function (ret) {
+    res.send("OK");
+  });
+});
+
+app.delete("/desconvidar/:idfesta/:idconvidado", function (req, res) {
+  knex("festa_convidado").del().where({
+    idconvidado: req.params.idconvidado,
+    idfesta: req.params.idfesta
+  }).then(function (ret) {
+    res.send("OK");
+  });
+});
+
 // db sanity
-knex.migrate.latest().then(function(){
-  
+knex.migrate.latest().then(function () {
+  console.log("App running");
   // app up and running
   app.listen(8080);
 });
