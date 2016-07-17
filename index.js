982f44a1f2782ccbac5c232d311a59d44046ccd8
Leonardo Silveira <sombriks@gmail.com>
2016-07-17 15:11:21 -0300

    adicionando suporte a banco de dados

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// 
+// imports 
 var express = require("express");
 var app     = express();
 
+var cfg     = require("./knexfile");
+var knex    = require("knex")(cfg.development);
+
+// static files
 app.use(express.static("www"));
 
-app.listen(8080);
+// db sanity
+knex.migrate.latest().then(function(){
+  
+  // app up and running
+  app.listen(8080);
+});
---
+++ migrations/20160717150435_esquema_inicial.js
@@ -0,0 +1,11 @@
+
+// o up é executado para alterar o esquema ou o estado do banco.
+exports.up = function(knex, Promise) {
+  // sempre retornar um builder ou uma promessa 
+  
+};
+
+// o down desfaz, sempre que possível, tudo o que o up faz.
+exports.down = function(knex, Promise) {
+  
+};
