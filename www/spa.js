254d2f36becd962645e80d7cc52252c17b65d740
Leonardo Silveira <sombriks@gmail.com>
2016-07-17 21:05:36 -0300

    configurando as rotas do aplicativo

--- www/spa.js
+++ www/spa.js
@@ -1,2 +1,36 @@
 // define angular module
-angular.module("festaspa", ["ngRoute"]);
+angular.module("festaspa", ["ngRoute"]).config(function($routeProvider){
+  
+  $routeProvider.when("/listar-convidados",{
+    templateUrl:"fragments/convidado/listar.html",
+    controller:"ListarConvidadosCtl",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.when("/listar-festas",{
+    templateUrl:"fragments/festa/listar.html",
+    controller:"ListarFestasCtl",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.when("/detalhe-convidado/:idconvidado",{
+    templateUrl:"fragments/convidado/detalhe.html",
+    controller:"DetalheConvidadoCtl",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.when("/detalhe-festa/:idfesta",{
+    templateUrl:"fragments/festa/detalhe.html",
+    controller:"DetalheFestaCtl",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.when("/convites-festa/:idfesta",{
+    templateUrl:"fragments/festa/convites.html",
+    controller:"ConvitesFestaCtl",
+    controllerAs:"ctl"
+  });
+
+  $routeProvider.otherwise("/listar-festas");
+
+});
